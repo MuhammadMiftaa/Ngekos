1e37162b10765b0cd36e5e2e356a7f1e7e8b4eb4
Muhammad Miftakul Salam <muhammadmiftakul.22101@mhs.unesa.ac.id>
2025-01-02 15:08:25 +0700

    add column of table city, category, boardinghouse

--- app/Models/City.php
+++ app/Models/City.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class City extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['image', 'name', 'slug'];
+
+    public function boardingHouses()
+    {
+        return $this->hasMany(BoardingHouse::class);
+    }
 }
