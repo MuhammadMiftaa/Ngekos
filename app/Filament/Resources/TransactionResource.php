11bebff4825266e647152591d9bebc8ea9066502
Muhammad Miftakul Salam <muhammadmiftakul.22101@mhs.unesa.ac.id>
2025-01-02 20:03:25 +0700

    add transaction crud

---
+++ app/Filament/Resources/TransactionResource.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\TransactionResource\Pages;
+use App\Filament\Resources\TransactionResource\RelationManagers;
+use App\Models\Transaction;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class TransactionResource extends Resource
+{
+    protected static ?string $model = Transaction::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('code')
+                    ->required(),
+                Forms\Components\TextInput::make('name')
+                    ->required(),
+                Forms\Components\TextInput::make('email')
+                    ->required(),
+                Forms\Components\TextInput::make('phone_number')
+                    ->required(),
+                Forms\Components\Select::make('boarding_house_id')
+                    ->required()
+                    ->relationship('boardingHouse', 'name'),
+                Forms\Components\Select::make('room_id')
+                    ->required()
+                    ->relationship('room', 'name'),
+                Forms\Components\Select::make('payment_method')
+                    ->options([
+                        'down_payment' => 'Down Payment',
+                        'full_payment' => 'Full Payment',
+                    ]),
+                Forms\Components\Select::make('payment_status')
+                    ->options([
+                        'pending' => 'Pending',
+                        'paid' => 'Paid',
+                    ]),
+                Forms\Components\Datepicker::make('start_date')
+                    ->required(),
+                Forms\Components\TextInput::make('duration')
+                    ->required()
+                    ->numeric(),
+                Forms\Components\TextInput::make('total_amount')
+                    ->required()
+                    ->prefix('IDR')
+                    ->numeric(),
+                Forms\Components\Datepicker::make('transaction_date')
+                    ->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('code'),
+                Tables\Columns\TextColumn::make('boardingHouse.name'),
+                Tables\Columns\TextColumn::make('room.name'),
+                Tables\Columns\TextColumn::make('name'),
+                Tables\Columns\TextColumn::make('payment_method'),
+                Tables\Columns\TextColumn::make('payment_status'),
+                Tables\Columns\TextColumn::make('start_date'),
+                Tables\Columns\TextColumn::make('duration'),
+                Tables\Columns\TextColumn::make('total_amount'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\ViewAction::make(),
+                Tables\Actions\DeleteAction::make(),
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListTransactions::route('/'),
+            'create' => Pages\CreateTransaction::route('/create'),
+            'edit' => Pages\EditTransaction::route('/{record}/edit'),
+        ];
+    }
+}
