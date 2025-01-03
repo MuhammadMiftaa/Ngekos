5e13a724588b2949fb248ccef6c573c5141a55a3
Muhammad Miftakul Salam <muhammadmiftakul.22101@mhs.unesa.ac.id>
2025-01-03 10:09:16 +0700

    configure check booking view and controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\CheckController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/check-booking', [CheckController::class, 'check'])->name('check-booking');
