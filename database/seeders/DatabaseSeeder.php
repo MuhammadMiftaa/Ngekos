d3f0986814dd00bf5d91ff14f0f120da4583ad53
Muhammad Miftakul Salam <muhammadmiftakul.22101@mhs.unesa.ac.id>
2025-01-19 08:19:42 +0700

    bug fix

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,6 +21,7 @@ class DatabaseSeeder extends Seeder
             BoardingHouseSeeder::class,
             RoomSeeder::class,
             BonusSeeder::class,
+            RoomImageSeeder::class,
         ]);
     }
 }
